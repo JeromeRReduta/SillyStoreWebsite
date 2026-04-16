29d9243265f81db116d6dc4bde79a2e1159b401d
JRReduta <jrreduta@yahoo.com>
2026-04-16 00:45:11 -0700

    (change) base layout, nav page

---
+++ src/nav/Nav.tsx
@@ -0,0 +1,11 @@
+import type { JSX } from "react";
+import css from "./nav.module.css";
+
+export default function Nav(): JSX.Element {
+  return (
+    <nav className={css.layout_nav}>
+      <div className={css.layout_nav_left}>LEFT</div>
+      <div className={css.layout_nav_right}>RIGHT</div>
+    </nav>
+  );
+}
