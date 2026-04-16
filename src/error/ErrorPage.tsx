79081cd5694c398619c0c91c7a42fdbaee553da1
JRReduta <jrreduta@yahoo.com>
2026-04-16 00:51:29 -0700

    (change) error page - content

--- src/error/ErrorPage.tsx
+++ src/error/ErrorPage.tsx
@@ -1,5 +1,18 @@
 import type { JSX } from "react";
+import css from "./error.module.css";
 
-export default function ErrorPage(/** TODO - props */): JSX.Element {
-  return <div>ErrorPage</div>;
+export default function ErrorPage(): JSX.Element {
+  return (
+    <section className={css.error_page}>
+      <div>
+        <img
+          width="200px"
+          height="200px"
+          src="/cat-stock-img.jpg"
+          alt="stock cat photo"
+        />
+      </div>
+      <div>You've made a routing error! Now you get stock cat photo.</div>
+    </section>
+  );
 }
