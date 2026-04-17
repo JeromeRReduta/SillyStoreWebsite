314adb1bab5b95b7c034041ebae6322b073b348b
JRReduta <jrreduta@yahoo.com>
2026-04-16 18:01:31 -0700

    (change) locked out - content

--- src/store/LockedOutPage.tsx
+++ src/store/LockedOutPage.tsx
@@ -1,5 +1,11 @@
 import type { JSX } from "react";
+import css from "./locked-out.module.css";
 
-export default function LockedOutPage(/** TODO - props */): JSX.Element {
-  return <div>LockedOutPage</div>;
+export default function LockedOutPage(): JSX.Element {
+  // todo: create cookie - locked-out
+  return (
+    <section className={css.locked_out}>
+      <h1>WHY WOULD YOU BUY UNETHICALLY SOURCED BONES YOU WEIRDO</h1>
+    </section>
+  );
 }
