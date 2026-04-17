0ca652271c69222308ea2a50a2433b9c34066987
JRReduta <jrreduta@yahoo.com>
2026-04-16 18:06:40 -0700

    (change) locked out page - content again

--- src/store/LockedOutPage.tsx
+++ src/store/LockedOutPage.tsx
@@ -1,11 +1,15 @@
 import type { JSX } from "react";
 import css from "./locked-out.module.css";
 
-export default function LockedOutPage(): JSX.Element {
-  // todo: create cookie - locked-out
+export default function LockedOutPage({
+  forbiddenProduct,
+}: {
+  forbiddenProduct: "HUMAN SKIN" | "UNETHICALLY SOURCED BONES";
+}): JSX.Element {
+  // todo: create cookie - locked_out_skin and locked_out_bones
   return (
     <section className={css.locked_out}>
-      <h1>WHY WOULD YOU BUY UNETHICALLY SOURCED BONES YOU WEIRDO</h1>
+      <h1>WHY WOULD YOU BUY {forbiddenProduct} YOU WEIRDO</h1>
     </section>
   );
 }
