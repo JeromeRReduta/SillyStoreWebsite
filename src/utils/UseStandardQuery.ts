9e7bb85721441e582b8bc785ef805d50e5ad3ad2
JRReduta <jrreduta@yahoo.com>
2026-04-19 19:42:07 -0700

    (change) store page - use standard query

---
+++ src/utils/UseStandardQuery.ts
@@ -0,0 +1,17 @@
+import { useQuery } from "@tanstack/react-query";
+
+export default function useStandardQuery<E>({
+    queryKeys,
+    requestFnAsync,
+}: {
+    queryKeys: string[];
+    requestFnAsync: () => Promise<Response>;
+}) {
+    return useQuery({
+        queryKey: queryKeys,
+        queryFn: async (): Promise<E | undefined> => {
+            const response: Response = await requestFnAsync();
+            return processResponseOrThrow(response);
+        },
+    });
+}
