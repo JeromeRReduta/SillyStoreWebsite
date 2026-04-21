8a548f756da76fc5a3f75222d4c382f8e9b0cb34
JRReduta <jrreduta@yahoo.com>
2026-04-20 20:13:12 -0700

    (change) bugfix - error and loading components also now use optional args properly

--- src/utils/Loading.tsx
+++ src/utils/Loading.tsx
@@ -1,5 +1,5 @@
 import css from "./loading.module.css";
 
-export default function Loading({ message = "Loading..." }: { message: string }) {
+export default function Loading({ message = "Loading..." }: { message?: string }) {
     return <section className={css.loading}>{message}</section>
 }
