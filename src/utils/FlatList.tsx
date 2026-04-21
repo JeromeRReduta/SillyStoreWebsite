028a95263a010882d4f7fd5ec104c48d4b39a05d
JRReduta <jrreduta@yahoo.com>
2026-04-20 20:12:33 -0700

    (change) bugfix - flatlist optional args are now actually optional

--- src/utils/FlatList.tsx
+++ src/utils/FlatList.tsx
@@ -5,8 +5,8 @@ import css from "./flat-list.module.css"
 /** Lovingly stolen from react native - credit to them */
 export default function FlatList<TData>(
     { listClassName = css.flat_list, listItemClassName = css.flat_list_item, data, renderItem, keyExtractor }: {
-        listClassName: string,
-        listItemClassName: string
+        listClassName?: string,
+        listItemClassName?: string
         data: TData[],
         renderItem: (elem: TData) => JSX.Element,
         keyExtractor: (elem: TData) => React.Key
