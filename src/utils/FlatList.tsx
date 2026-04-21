c3ed27a11c72eef8e4b6559641450346e0b7de71
JRReduta <jrreduta@yahoo.com>
2026-04-20 20:03:10 -0700

    (change) flat list now injects classnames

--- src/utils/FlatList.tsx
+++ src/utils/FlatList.tsx
@@ -4,13 +4,15 @@ import css from "./flat-list.module.css"
 
 /** Lovingly stolen from react native - credit to them */
 export default function FlatList<TData>(
-    { data, renderItem, keyExtractor }: {
+    { listClassName = css.flat_list, listItemClassName = css.flat_list_item, data, renderItem, keyExtractor }: {
+        listClassName: string,
+        listItemClassName: string
         data: TData[],
         renderItem: (elem: TData) => JSX.Element,
         keyExtractor: (elem: TData) => React.Key
     }
 ): JSX.Element {
-    return <section className={css.flat_list}>
-        {data.map((elem) => <div key={keyExtractor(elem)} className={css.flat_list_item}>{renderItem(elem)}</div>)}
+    return <section className={listClassName}>
+        {data.map((elem) => <div key={keyExtractor(elem)} className={listItemClassName}>{renderItem(elem)}</div>)}
     </section>
 }
