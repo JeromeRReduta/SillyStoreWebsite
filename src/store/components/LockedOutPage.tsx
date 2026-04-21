bf7636ce95e8eef41c9eb54e1fccc7f121616b82
JRReduta <jrreduta@yahoo.com>
2026-04-20 20:00:06 -0700

    (change) prettier run

--- src/store/components/LockedOutPage.tsx
+++ src/store/components/LockedOutPage.tsx
@@ -3,16 +3,24 @@ import css from "../css/locked-out.module.css";
 import type { JSX } from "react";
 import FadeInElem from "../../animated/FadeInElem";
 
-
 export default function LockedOutPage(): JSX.Element {
-    const [cookies, _setCookie, removeCookie] = useCookies<"locked_out", { locked_out: string }>(["locked_out"]);
-    const forbiddenProduct: string | undefined = cookies.locked_out
+    const [cookies, _setCookie, removeCookie] = useCookies<
+        "locked_out",
+        { locked_out: string }
+    >(["locked_out"]);
+    const forbiddenProduct: string | undefined = cookies.locked_out;
     return (
         <section className={css.locked_out}>
             <h1>WHY WOULD YOU BUY {forbiddenProduct} YOU WEIRDO</h1>
             <FadeInElem>
-                <button className={css.locked_out_button}
-                    onClick={() => { removeCookie("locked_out") }} >"Ok I'm sorry, let me back in"</button>
+                <button
+                    className={css.locked_out_button}
+                    onClick={() => {
+                        removeCookie("locked_out");
+                    }}
+                >
+                    "Ok I'm sorry, let me back in"
+                </button>
             </FadeInElem>
         </section>
     );
