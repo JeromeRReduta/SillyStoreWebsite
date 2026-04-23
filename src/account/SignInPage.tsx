2e9532279e21225de7d3777d2677038783d1f303
JRReduta <jrreduta@yahoo.com>
2026-04-22 21:37:40 -0700

    (refactor) sign-in page - changed console.log to frontendLogger, (change) store page/product card - now has nice rounded corners

--- src/account/SignInPage.tsx
+++ src/account/SignInPage.tsx
@@ -3,30 +3,38 @@ import { Link, useNavigate } from "react-router";
 import css from "./sign-in.module.css";
 import paths from "../routing/Paths";
 import LabeledTextInput from "../utils/LabeledTextInput";
+import frontendLogger from "../configs/frontendLogger";
 
 type SupportedMethods = "LOGIN" | "REGISTER";
 
-
-export default function SignInPage({ method }: { method: SupportedMethods }): JSX.Element {
+export default function SignInPage({
+    method,
+}: {
+    method: SupportedMethods;
+}): JSX.Element {
     const navigate = useNavigate();
-    // const action: (formData: FormData) => Promise<void> = method === "LOGIN" ? loginAsync : registerAsync;
     const action: (formData: FormData) => Promise<void> = mockSignIn;
     const onSubmit: () => void = () => navigate("/" + paths.store.base);
     const labelNames: string[] = ["Email", "Username", "Password"];
 
-    return <section className={css.sign_in}>
-        <SignInForm action={action} onSubmit={onSubmit} labelNames={labelNames} />
-        <LinkToOther method={method} />
-    </section>
-
+    return (
+        <section className={css.sign_in}>
+            <SignInForm
+                action={action}
+                onSubmit={onSubmit}
+                labelNames={labelNames}
+            />
+            <LinkToOther method={method} />
+        </section>
+    );
 }
 
-
-async function mockSignIn(formData: FormData): Promise<void> { // TODO: delete?
-    console.log("signing in!");
+async function mockSignIn(formData: FormData): Promise<void> {
+    // TODO: delete?
+    frontendLogger.debug("signing in!");
     const entries = formData.entries();
     for (const entry of entries) {
-        console.log(entry)
+        frontendLogger.debug(entry);
     }
 }
 
@@ -36,34 +44,36 @@ interface SignInFormInfo {
     readonly onSubmit: () => void | Promise<void>;
 }
 
-function SignInForm({ action, labelNames, onSubmit }: SignInFormInfo): JSX.Element {
-    return <form
-        action={action}
-        onSubmit={onSubmit}
-        className={css.sign_in_form}
-    >
-        {labelNames.map((name) =>
-            <LabeledTextInput
-                label={name}
-                text={name}
-                autoComplete="off"
-                required={true}
-                key={name}
-            />
-        )}
-        <button className={css.sign_in_submit_button}>Submit</button>
-    </form >
+function SignInForm({
+    action,
+    labelNames,
+    onSubmit,
+}: SignInFormInfo): JSX.Element {
+    return (
+        <form action={action} onSubmit={onSubmit} className={css.sign_in_form}>
+            {labelNames.map((name) => (
+                <LabeledTextInput
+                    label={name}
+                    text={name}
+                    autoComplete="off"
+                    required={true}
+                    key={name}
+                />
+            ))}
+            <button className={css.sign_in_submit_button}>Submit</button>
+        </form>
+    );
 }
 
 function LinkToOther({ method }: { method: SupportedMethods }): JSX.Element {
-    return method === "LOGIN"
-        ? <div className={css.sign_in_link_to_other}>
+    return method === "LOGIN" ? (
+        <div className={css.sign_in_link_to_other}>
             New? Register <Link to={"../" + paths.account.register}>here</Link>.
         </div>
-        : <div className={css.sign_in_link_to_other}>
-            Have an account? Login <Link to={"../" + paths.account.login}>here</Link>.
+    ) : (
+        <div className={css.sign_in_link_to_other}>
+            Have an account? Login{" "}
+            <Link to={"../" + paths.account.login}>here</Link>.
         </div>
-};
-
-
-
+    );
+}
