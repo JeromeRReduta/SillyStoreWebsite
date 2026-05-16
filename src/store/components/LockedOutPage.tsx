5492bf5284ee95c944d1caabd8c0fe3ecc909c66
JRReduta <jrreduta@yahoo.com>
2026-05-16 06:14:45 -0700

    (change) locked out page - redirected to home page when you click button

--- src/store/components/LockedOutPage.tsx
+++ src/store/components/LockedOutPage.tsx
@@ -2,12 +2,16 @@ import { useCookies } from "react-cookie";
 import css from "../css/locked-out.module.css";
 import type { JSX } from "react";
 import FadeInElem from "../../animated/FadeInElem";
+import frontendConfigs from "../../configs/FrontendConfigs";
+import { useNavigate } from "react-router";
 
 export default function LockedOutPage(): JSX.Element {
     const [cookies, _setCookie, removeCookie] = useCookies<
         "locked_out",
         { locked_out: string }
     >(["locked_out"]);
+    const navigate = useNavigate();
+
     const forbiddenProduct: string | undefined = cookies.locked_out;
     return (
         <section className={css.locked_out}>
@@ -17,6 +21,9 @@ export default function LockedOutPage(): JSX.Element {
                     className={css.locked_out_button}
                     onClick={() => {
                         removeCookie("locked_out");
+                        void navigate(
+                            frontendConfigs.absolutePaths.internal.store,
+                        );
                     }}
                 >
                     "Ok I'm sorry, let me back in"
