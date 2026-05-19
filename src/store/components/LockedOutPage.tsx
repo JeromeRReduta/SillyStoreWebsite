d8401b76db779e309860b2ceb0135a0b94c2e195
JRReduta <jrreduta@yahoo.com>
2026-05-19 00:30:05 -0700

    (refactor) cookie hook - locked out page

--- src/store/components/LockedOutPage.tsx
+++ src/store/components/LockedOutPage.tsx
@@ -1,21 +1,17 @@
-import { useCookies } from "react-cookie";
-import css from "../css/locked-out.module.css";
 import type { JSX } from "react";
+import { useNavigate } from "react-router";
 import FadeInElem from "../../animated/FadeInElem";
 import frontendConfigs from "../../configs/FrontendConfigs";
-import { useNavigate } from "react-router";
+import useWebsiteCookies from "../../utils/services/useWebsiteCookies";
+import css from "../css/locked-out.module.css";
 
 export default function LockedOutPage(): JSX.Element {
-    const [cookies, _setCookie, removeCookie] = useCookies<
-        "locked_out",
-        { locked_out: string }
-    >(["locked_out"]);
+    const [{ locked_out }, _setCookie, removeCookie] = useWebsiteCookies();
     const navigate = useNavigate();
 
-    const forbiddenProduct: string | undefined = cookies.locked_out;
     return (
         <section className={css.locked_out}>
-            <h1>WHY WOULD YOU BUY {forbiddenProduct} YOU WEIRDO</h1>
+            <h1>WHY WOULD YOU BUY {locked_out} YOU WEIRDO</h1>
             <FadeInElem>
                 <button
                     className={css.locked_out_button}
